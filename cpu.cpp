42c30c3d714eb3434ed94082c53d858369191b70
otr1v <100768041+otr1v@users.noreply.github.com>
2023-01-23 01:54:20 +0300

    Update cpu.cpp
    Added CPU structure, fixed some details like unknown names of variables

--- cpu.cpp
+++ cpu.cpp
@@ -1,16 +1,19 @@
 #include "cpu.h"
+#include "funccpu.h"
+
 
 int main()
 {
-
+    CPU cpu = {};
     FILE * asmcode = NULL;
-    int registers[AMOUNT_OF_REGISTERS] = {1, 2};
+    cpu.registers[0] = 1;
+    cpu.registers[1] = 2;
+    //TODO create RAM, create regs
     int ram[10] = {1, 2, 3, 4, 5, 6};
     stack_type stack = {};
-    stackCreator_(&stack, 5);
-    ReadAsm(asmcode, &stack, registers, ram);
+    stackCreator_(&(cpu.stack), 5);
+    elem_t d = 5;
+    stackPush(&(cpu.stack), &d);
+    ReadAsm(asmcode, &cpu);
     return 0;
 }
-
-//==============================================================================
-

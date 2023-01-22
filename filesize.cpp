b192014bd0815c9e6e3c257aa7efa4a3f6c8e381
otr1v <100768041+otr1v@users.noreply.github.com>
2023-01-23 01:52:08 +0300

    Create filesize.cpp

---
+++ filesize.cpp
@@ -0,0 +1,10 @@
+#include "filesize.h"
+
+int FileSize(FILE* fp)
+{
+    struct stat buffer = {};
+    int res_fstat = fstat(fileno(fp), &buffer);
+    CHECK_ERR(res_fstat == -1, "can't fill the structure stat", ERR_FSTAT);
+    int size_buf = buffer.st_size;
+    return size_buf;
+}

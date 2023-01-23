6fc0057638ebda3304d5aaa3e01beba0b02a3f71
otr1v <100768041+otr1v@users.noreply.github.com>
2023-01-24 00:42:13 +0300

    Update cpu.cpp

--- cpu.cpp
+++ cpu.cpp
@@ -6,14 +6,15 @@ int main()
 {
     CPU cpu = {};
     FILE * asmcode = NULL;
-    cpu.registers[0] = 1;
-    cpu.registers[1] = 2;
-    //TODO create RAM, create regs
-    int ram[10] = {1, 2, 3, 4, 5, 6};
-    stack_type stack = {};
+    // cpu.registers[0] = 1;
+    // cpu.registers[1] = 2;
+    // int ram[10] = {1, 2, 3, 4, 5, 6};
+    CreateCpu(&cpu);
     stackCreator_(&(cpu.stack), 5);
     elem_t d = 5;
     stackPush(&(cpu.stack), &d);
     ReadAsm(asmcode, &cpu);
     return 0;
 }
+
+

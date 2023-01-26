dc34ef5344e61bc98ecf3ebaeb22aa0f015a6412
otr1v <100768041+otr1v@users.noreply.github.com>
2023-01-26 20:12:53 +0300

    Update asm.h

--- asm.h
+++ asm.h
@@ -4,67 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <strings.h>
+#include "enum.h"
 
-#define CHECK_ERR(condition, message_error, err_code) \
-                do{                                    \
-                        if (condition){                 \
-                            printf("%s", message_error); \
-                            return err_code;              \
-                        }                                  \
-                } while(0)
-// #define DEF_CMD(name, num, arg) \
-//         CMD_##name = num,
-// enum 
-// {
-//     #include "cmd.h"
-// }
-// #undef DEF_CMD
-
-// #define DEF_CMD(name, num, arg) \
-//             if (strcmp(cmd, #name) == 0) \
-//                 {code[ip++] = num;   \
-//                 if (arg) GetArgs();}   \
-//                 else
-
-;
 
 const int MAX_LABEL_SIZE      = 300;
 const int MAX_COMMANDS        = 1000;
-const int AMOUNT_OF_REGISTERS = 20;
-const int REG_RAX             = 0;
-const int REG_RBX             = 1;
-const int REG_RCX             = 2;
-const int REG_RDX             = 3;
-const int REG_REX             = 4;
-const int REG_RFX             = 5;
 const int MASK_IMMED          = 1 << 6;
 const int MASK_REGISTER       = 1 << 7;
 const int MASK_RAM            = 1 << 8;
 const int AMOUNT_OF_RAM       = 15;
 const int MAX_NAME_REG_SIZE   = 6;
 const int MAX_SIZE_OF_COMMAND = 10;
-enum 
-{
-    CMD_PUSH = 1,
-    CMD_POP  = 2,
-    CMD_ADD  = 3,
-    CMD_SUB  = 4,
-    CMD_MUL  = 5,
-    CMD_DIV  = 6,
-    CMD_OUT  = 7,
-    CMD_IN   = 8,
-    CMD_HLT  = 9,
-    CMD_JMP  = 10,
-    CMD_SQRT = 11,
-    CMD_CPY  = 12,
-    CMD_JB   = 13,
-    CMD_JE   = 14,
-    CMD_CALL = 15,
-    CMD_FACT = 16,
-    NO_MEMORY,
-    ERR_OPEN_FILE, 
-    BAD_FREAD
-};
+
 
 typedef struct 
 {

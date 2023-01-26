0b1edea996ce7ae07f561f2b9ba0960531bfe4b8
otr1v <100768041+otr1v@users.noreply.github.com>
2023-01-26 20:15:54 +0300

    Update cpufunc.h
    added DSL

--- cpufunc.h
+++ cpufunc.h
@@ -1,6 +1,7 @@
 #include "cpu.h"
 #include "filesize.cpp"
 #define QUADRO_EQ
+
 void CreateCpu(CPU* cpu)
 {
     for (int i = 0; i < AMOUNT_OF_REGISTERS; i++)
@@ -46,190 +47,198 @@ int ReadAsm(FILE * asmcode, CPU* cpu)
         // printf("%d\n", ip);
         // printf("In stack %d\n", cpu->stack->data[cpu->stack->size - 1]);
         //printf("ip%d", ip);
+                //printf("cnsdc");
 
-        switch (cpu->code[ip] & CMD_MASK)
-        {
-        case CMD_PUSH:
-           // printf("ip");
-            val = GetArgs(&ip, cpu);
-            printf("push value is %d\n", val);
-            printf("hf");
-            stackPush(&(cpu->stack), &val);
-                        printf("hf");
+        switch (cpu->code[ip] & CMD_MASK){
+        // {
+        // case CMD_PUSH:
+        //    // printf("ip");
+        //     val = GetArgs(&ip, cpu);
+        //     printf("push value is %d\n", val);
+        //     printf("hf");
+        //     stackPush(&(cpu->stack), &val);
+        //     printf("hf");
 
-            //printf("code : %d\n", cpu->stack->data[cpu->stack->size - 1]);
-            //ip += 2;
-            break;
-        case CMD_ADD:
-            // printf("In stack %d\n", cpu->stack->data[cpu->stack->size]);
-            //stackPop(cpu->stack, err, &pop_value1);
-            // printf("In stack %d\n", cpu->stack->data[cpu->stack->size]);
-            //stackPop(cpu->stack, err, &pop_value2);
-            // printf("pop value1 = %d\n", pop_value1);
-            // printf("pop value2 = %d\n", pop_value2);
-                        printf("hf");
+        //     //printf("code : %d\n", cpu->stack->data[cpu->stack->size - 1]);
+        //     //ip += 2;
+        //     break;
+        // case CMD_ADD:
+        //     // printf("In stack %d\n", cpu->stack->data[cpu->stack->size]);
+        //     //stackPop(cpu->stack, err, &pop_value1);
+        //     // printf("In stack %d\n", cpu->stack->data[cpu->stack->size]);
+        //     //stackPop(cpu->stack, err, &pop_value2);
+        //     // printf("pop value1 = %d\n", pop_value1);
+        //     // printf("pop value2 = %d\n", pop_value2);
+        //     // printf("hf");
 
-            pop_value = stackPop(&cpu->stack, err) + stackPop(&cpu->stack, err);
-            //printf("pop value = %d\n", pop_value);
-            stackPush(&cpu->stack, &pop_value);
-            printf( "addval %d\n", (cpu->stack.data[cpu->stack.size - 1]));
+        //     pop_value = stackPop(&cpu->stack, err) + stackPop(&cpu->stack, err);
+        //     //printf("pop value = %d\n", pop_value);
+        //     stackPush(&cpu->stack, &pop_value);
+        //     //printf( "addval %d\n", (cpu->stack.data[cpu->stack.size - 1]));
 
-            ip++;
-            break;
-        case CMD_DIV:
-            if (cpu->stack.data[cpu->stack.size - 1] != 0)
-            {
-                first_pop = stackPop(&cpu->stack, err);
-                second_pop = stackPop(&cpu->stack, err);
-                div_result = (int)(second_pop / first_pop);
-                stackPush(&cpu->stack, &div_result);
-            }
-            else
-            {
-                printf("attempt to divide by 0\n");
-            }
-            printf("divvvv %d", div_result);
-            ip++;
-            break;
-        case CMD_POP:
-            ip++;
-            cmd_pop_value = stackPop(&(cpu->stack), err);
-            printf("code[ip] is%d\n", cmd_pop_value);
-                        printf("hf");
-            cpu->registers[cpu->code[ip++]] = cmd_pop_value;
-            printf("reg %d", cpu->registers[3]);
-            break;
-        case CMD_HLT:
-            #ifdef QUADRO_EQ
-                if (cpu->registers[REG_REX] == 0)
-                {
-                    printf("no roots\n");
-                }
-                else
-                {
-                    printf("the first root %d\n the second root %d\n", cpu->registers[REG_REX], cpu->registers[REG_RFX]);
-                }
-            #endif
-            return 0;
-            break;
-        case CMD_JMP:
-            ip++;
-            printf("ipiii%d", ip);
-            ip = cpu->code[ip];
-            printf("look%d", ip);
-            break;
-        case CMD_SQRT:
-            pop_value = stackPop(&cpu->stack, err);
-            printf("wtf");
-            if (pop_value < 0)
-            {
-                printf("sqrt of number that < 0");
-            }
-            else 
-            {
-                sqrt_result = MySqrt(pop_value);
-                //sqrt_result = (int)(pop_value / 2);
-                stackPush(&cpu->stack, &sqrt_result);
-            }
+        //     ip++;
+        //     break;
+        // case CMD_DIV:
+        //     if (cpu->stack.data[cpu->stack.size - 1] != 0)
+        //     {
+        //         first_pop = stackPop(&cpu->stack, err);
+        //         second_pop = stackPop(&cpu->stack, err);
+        //         div_result = (int)(second_pop / first_pop);
+        //         stackPush(&cpu->stack, &div_result);
+        //     }
+        //     else
+        //     {
+        //         printf("attempt to divide by 0\n");
+        //     }
+        //     printf("divvvv %d", div_result);
+        //     ip++;
+        //     break;
+        // case CMD_POP:
+        //     //ip++;
+        //     cmd_pop_value = stackPop(&(cpu->stack), err);
+        //     val = GetArgs(&ip, cpu);
+        //     cpu->registers[val] = cmd_pop_value;
+        //     printf("reg %d", cpu->registers[3]);
+        //     break;
+        // case CMD_HLT:
+        //     #ifdef QUADRO_EQ
+        //         if (cpu->registers[REG_REX] == 0)
+        //         {
+        //             printf("no roots\n");
+        //         }
+        //         else
+        //         {
+        //             printf("the first root %d\n the second root %d\n", cpu->registers[REG_REX], cpu->registers[REG_RFX]);
+        //         }
+        //     #endif
+        //     return 0;
+        //     break;
+        // case CMD_JMP:
+        //     ip++;
+        //     printf("ipiii%d", ip);
+        //     ip = cpu->code[ip];
+        //     printf("look%d", ip);
+        //     break;
+        // case CMD_SQRT:
+        //     pop_value = stackPop(&cpu->stack, err);
+        //     if (pop_value < 0)
+        //     {
+        //         printf("sqrt of number that < 0");
+        //     }
+        //     else 
+        //     {
+        //         sqrt_result = MySqrt(pop_value);
+        //         stackPush(&cpu->stack, &sqrt_result);
+        //     }
             
-            ip++;
-            break;
-        case CMD_CPY:
-            pop_value = stackPop(&cpu->stack, err);
-            stackPush(&cpu->stack, &pop_value);
-            stackPush(&cpu->stack, &pop_value);
-            printf("CPY ARG%d and %d\n", cpu->stack.data[cpu->stack.size - 1], cpu->stack.data[cpu->stack.size - 2]);
-            ip++;
-            break;
-        case CMD_MUL:
-            pop_value = (stackPop(&cpu->stack, err)) * (stackPop(&cpu->stack, err));
-            printf("whatlol");
-            stackPush(&cpu->stack, &pop_value);
-            printf("mulval %d\n", cpu->stack.data[cpu->stack.size - 1]);
-            printf("rex %d\n", cpu->registers[REG_REX]);
-            ip++;
-            break;
-        case CMD_JB:
-            if (cpu->registers[REG_RDX] > 0)
-            {
-                ip++;
-                ip = cpu->code[ip] + 1;
-                printf("ipp %d", ip);
-            }
-            else
-            {
-                ip += 2;
-            }
-            break;
-        case CMD_JE:
-            #ifdef QUADRO_EQ
-                ip++;
-                // if (cpu->registers[REG_RAX] == 0)
-                // {
-                //     ip = cpu->code[ip] - 1;
-                // }
-                if (counter == 1)
-                {
-                    if (cpu->registers[REG_RDX] == 0)
-                        {
+        //     ip++;
+        //     break;
+        // case CMD_CPY:
+        //     pop_value = stackPop(&cpu->stack, err);
+        //     stackPush(&cpu->stack, &pop_value);
+        //     stackPush(&cpu->stack, &pop_value);
+        //     //printf("CPY ARG%d and %d\n", cpu->stack.data[cpu->stack.size - 1], cpu->stack.data[cpu->stack.size - 2]);
+        //     ip++;
+        //     break;
+        // case CMD_MUL:
+        //     pop_value = (stackPop(&cpu->stack, err)) * (stackPop(&cpu->stack, err));
+        //     stackPush(&cpu->stack, &pop_value);
+        //     // printf("mulval %d\n", cpu->stack.data[cpu->stack.size - 1]);
+        //     // printf("rex %d\n", cpu->registers[REG_REX]);
+        //     ip++;
+        //     break;
+        // case CMD_JB:
+        //     if (cpu->registers[REG_RDX] > 0)
+        //     {
+        //         ip++;
+        //         ip = cpu->code[ip] + 1;
+        //         printf("ipp %d", ip);
+        //     }
+        //     else
+        //     {
+        //         ip += 2;
+        //     }
+        //     break;
+        // case CMD_JE:
+        //     #ifdef QUADRO_EQ
+        //         ip++;
+        //         // if (cpu->registers[REG_RAX] == 0)
+        //         // {
+        //         //     ip = cpu->code[ip] - 1;
+        //         // }
+        //         if (counter == 1)
+        //         {
+        //             if (cpu->registers[REG_RDX] == 0)
+        //                 {
                             
-                            ip = cpu->code[ip] - 1;
-                        }
-                    else
-                    {
-                        ip++;
-                    }
-                    counter++;
-                }
-                else if (counter == 0)
-                {
-                    if ((cpu->registers[REG_RAX] == 0) && 
-                        (cpu->registers[REG_RBX] == 0) &&
-                        (cpu->registers[REG_RCX] == 0))
-                        {
-                            printf("infinity roots");
-                            ip = cpu->code[ip] - 1;
-                            insidecounter++;
-                        }
-                    else if((cpu->registers[REG_RAX] == 0) && 
-                        (cpu->registers[REG_RBX] == 0) &&
-                        (cpu->registers[REG_RCX] != 0))
-                        {
-                            printf("no roots");
-                        }
-                    else if((cpu->registers[REG_RAX] == 0) && 
-                        (cpu->registers[REG_RBX] != 0) &&
-                        (cpu->registers[REG_RCX] != 0))
-                        {
-                            ip = cpu->code[ip] - 1;
-                        }
-                    else if ((cpu->registers[REG_RAX] != 0) && (insidecounter == 0))
-                    {
-                        ip++;
-                        counter++;
-                    }
-                    else
-                    {
+        //                     ip = cpu->code[ip] - 1;
+        //                 }
+        //             else
+        //             {
+        //                 ip++;
+        //             }
+        //             counter++;
+        //         }
+        //         else if (counter == 0)
+        //         {
+        //             if ((cpu->registers[REG_RAX] == 0) && 
+        //                 (cpu->registers[REG_RBX] == 0) &&
+        //                 (cpu->registers[REG_RCX] == 0))
+        //                 {
+        //                     printf("infinity roots");
+        //                     ip = cpu->code[ip] - 1;
+        //                     insidecounter++;
+        //                 }
+        //             else if((cpu->registers[REG_RAX] == 0) && 
+        //                 (cpu->registers[REG_RBX] == 0) &&
+        //                 (cpu->registers[REG_RCX] != 0))
+        //                 {
+        //                     printf("no roots");
+        //                 }
+        //             else if((cpu->registers[REG_RAX] == 0) && 
+        //                 (cpu->registers[REG_RBX] != 0) &&
+        //                 (cpu->registers[REG_RCX] != 0))
+        //                 {
+        //                     ip = cpu->code[ip] - 1;
+        //                 }
+        //             else if ((cpu->registers[REG_RAX] != 0) && (insidecounter == 0))
+        //             {
+        //                 ip++;
+        //                 counter++;
+        //             }
+        //             else
+        //             {
 
-                    }
+        //             }
                     
-                }
-            #endif
-            break;
-        case CMD_SUB:
-            first_pop = stackPop(&cpu->stack, err);
-            second_pop = stackPop(&cpu->stack, err);
-            sub_result = second_pop - first_pop;
-            stackPush(&cpu->stack, &sub_result);
-            printf("sub res %d\n", sub_result);
-            ip++;
-            break;
+        //         }
+        //     #endif
+        //     break;
+        // case CMD_SUB:
+        //     first_pop = stackPop(&cpu->stack, err);
+        //     second_pop = stackPop(&cpu->stack, err);
+        //     sub_result = second_pop - first_pop;
+        //     stackPush(&cpu->stack, &sub_result);
+        //     printf("sub res %d\n", sub_result);
+        //     ip++;
+        //     break;
+        // default:
+        //     break;
+        // }
+        #define DEF_CMD(name, num, arg, code)   \
+            case num:                            \
+                code                              \
+                ip++;                              \
+                break;
+        #include "cmd.h"
         default:
             break;
-        }
+        #undef DEF_CMD
+        }        
     }
     for (int i = 0; i < 7; i++)
     {
+        printf("cnsdc");
         printf("%d.%d\n", i, cpu->stack.data[i]);
         printf("cpu->registers %d\n", cpu->registers[i]);
     }
@@ -237,8 +246,6 @@ int ReadAsm(FILE * asmcode, CPU* cpu)
     return 0;
     //printf("stack");
 }
-
-//=================================================================================
 
 
 //=====================================================================================
@@ -250,11 +257,11 @@ int GetArgs(int* ip, CPU* cpu)
     //printf("lol");
     if (cmd & ARG_IMMED)
     {
-        arg = cpu->code[(*(ip))++];
+        arg = cpu->code[(*(ip))];
     }
     if (cmd & ARG_REG)
     {
-        arg = cpu->registers[cpu->code[(*(ip))++]];
+        arg = cpu->registers[cpu->code[(*(ip))]];
     }
     if (cmd & ARG_RAM)
     {
